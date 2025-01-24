58ae2f6c979a301501e2efcae907d1f73d10d9ef
Patrick Kelly <patdkelly5@gmail.com>
2025-01-24 18:06:55 +1100

    Implemented description and state handlers, added mising iterator keys

--- swole-patrol/src/components/ExerciseCard.jsx
+++ swole-patrol/src/components/ExerciseCard.jsx
@@ -25,6 +25,16 @@ export default function ExerciseCard(props) {
         <p className="capitalize">{exercise.muscles.join(" & ")}</p>
       </div>
 
+      <div className="flex flex-col bg-slate-950 rounded gap-2">
+        {exercise.description.split("___").map((val) => {
+          return (
+            <div key={val} className="text-sm">
+              {val}
+            </div>
+          );
+        })}
+      </div>
+
       <div className="grid grid-cols-2 sm:grid-cols-4 sm:place-items-center gap-2">
         {["reps", "rest", "tempo"].map((info) => {
           return (
