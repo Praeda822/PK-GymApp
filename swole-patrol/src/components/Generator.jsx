29a3961b85baa1d55b018ddef50ce4ed2c567a89
Patrick Kelly <patdkelly5@gmail.com>
2025-01-24 16:30:33 +1100

    Dynamic menu filtering implemented

--- swole-patrol/src/components/Generator.jsx
+++ swole-patrol/src/components/Generator.jsx
@@ -1,7 +1,7 @@
 // rfc
 import React, { useState } from "react";
 import SectionWrapper from "./SectionWrapper";
-import { WORKOUTS } from "../utils/swoldier";
+import { SCHEMES, WORKOUTS } from "../utils/swoldier";
 
 function Header(props) {
   const { index, title, description } = props;
@@ -29,11 +29,32 @@ export default function Generator() {
   // Remembering to import useState from react...
   // So the syntax ends up being: const [variableName, setVariableName] = useState(initialValue)
   const [showModal, setShowModal] = useState(false);
+  const [poison, setPosion] = useState("individual");
+  const [muscles, setMuscles] = useState([]);
+  const [goal, setGoal] = useState("strength_power");
 
   function toggleModal() {
     // Now, instead of directly changing the variable, I use the setter function to update the state
     // showModal = !showModal;
     setShowModal(!showModal);
+  }
+
+  function updateMuscles(muscleGroup) {
+    if (muscles.length > 2) {
+      return;
+    }
+
+    if (poison !== "individual") {
+      setMuscles([muscleGroup]);
+      return;
+    }
+
+    if (muscles.includes(muscleGroup)) {
+      setMuscles(muscles.filter((val) => val !== muscleGroup));
+      return;
+    }
+
+    setMuscles([...muscles, muscleGroup]);
   }
 
   // SectionWrapper will contain the generator for child elements that WON"T be repeated
@@ -51,7 +72,13 @@ export default function Generator() {
         {Object.keys(WORKOUTS).map((type, typeIndex) => {
           return (
             <button
-              className="bg-slate-950 border border-red-400 duration-200 hover:border-red-700 py-3 rounded-lg"
+              onClick={() => {
+                setPosion(type);
+              }}
+              className={
+                "bg-slate-950 border duration-200 hover:border-red-700 py-3 rounded-lg " +
+                (type === poison ? "border-red-700" : "border-red-400")
+              }
               key={typeIndex}
             >
               <p className="capitalize">{type.replaceAll("_", " ")}</p>
@@ -59,6 +86,7 @@ export default function Generator() {
           );
         })}
       </div>
+
       <Header
         index={"02"}
         title={"Lock-on Targets"}
@@ -72,7 +100,55 @@ export default function Generator() {
           <p>Select muscle groups</p>
           <i className="fa-solid absolute right-3 top-1/2 -translate-y-1/2 fa-caret-down"></i>
         </button>
-        {showModal && <div>modal</div>}
+        {showModal && (
+          <div className="flex flex-col px-3 pb-3">
+            {(poison === "individual"
+              ? WORKOUTS[poison]
+              : Object.keys(WORKOUTS[poison])
+            ).map((muscleGroup, muscleGroupIndex) => {
+              return (
+                <button
+                  onClick={() => {
+                    updateMuscles(muscleGroup);
+                  }}
+                  key={muscleGroupIndex}
+                  className={
+                    "hover:text-red-700 duration-200 " +
+                    (muscles.includes(muscleGroup) ? "text-red-700" : "")
+                  }
+                >
+                  <p className="uppercase">
+                    {muscleGroup.replaceAll("_", " ")}
+                  </p>
+                </button>
+              );
+            })}
+          </div>
+        )}
+      </div>
+
+      <Header
+        index={"03"}
+        title={"Conquer your goals"}
+        description={"Select your ultimate objective."}
+      />
+      <div className="grid grid-cols-3 gap-4">
+        {Object.keys(SCHEMES).map((scheme, schemeIndex) => {
+          return (
+            <button
+              onClick={() => {
+                setGoal(scheme);
+              }}
+              className={
+                "bg-slate-950 border duration-200 hover:border-red-700 py-3 rounded-lg " +
+                (scheme === goal ? "border-red-700" : "border-red-400")
+              }
+              key={schemeIndex}
+            >
+              <p className="capitalize">{scheme.replaceAll("_", " ")}</p>
+            </button>
+          );
+        })}
       </div>
     </SectionWrapper>
   );
