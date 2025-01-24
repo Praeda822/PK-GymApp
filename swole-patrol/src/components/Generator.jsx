da6538eef32cc506529811c4409a6446ad4e43af
Patrick Kelly <patdkelly5@gmail.com>
2025-01-24 15:37:36 +1100

    Conditional dropdown rendering component | squared caret by flex-col'ing it lol

--- swole-patrol/src/components/Generator.jsx
+++ swole-patrol/src/components/Generator.jsx
@@ -1,15 +1,62 @@
 // rfc
 import React from "react";
 import SectionWrapper from "./SectionWrapper";
+import { WORKOUTS } from "../utils/swoldier";
+
+function Header(props) {
+  const { index, title, description } = props;
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center justify-center gap-2">
+        <p className="text-3xl sm-text-4xl md:text-5xl font-semibold text-slate-400">
+          {index}
+        </p>
+        <h4 className="text-xl sm:text-2xl md:text-3xl">{title}</h4>
+      </div>
+      <p className="text-sm sm:text-base mx-auto">{description}</p>
+    </div>
+  );
+}
 
 export default function Generator() {
+  // Handles the dropdown for muscle groups
+  let showModal = false;
+
   // SectionWrapper will contain the generator for child elements that WON"T be repeated
   return (
     <SectionWrapper
       header={"generate your workout"}
       title={["It's", "Huge", "o'clock"]}
     >
-      sfsdf
+      <Header
+        index={"01"}
+        title={"Pick your poison"}
+        description={"Select the workout you wish to endure"}
+      />
+      <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
+        {Object.keys(WORKOUTS).map((type, typeIndex) => {
+          return (
+            <button
+              className="bg-slate-950 border border-red-400 duration-200 hover:border-red-700 py-3 rounded-lg"
+              key={typeIndex}
+            >
+              <p className="capitalize">{type.replaceAll("_", " ")}</p>
+            </button>
+          );
+        })}
+      </div>
+      <Header
+        index={"02"}
+        title={"Lock-on Targets"}
+        description={"Select your muscle groups"}
+      />
+      <div className="bg-slate-950  border bordcer-solid border-red-400 rounded-lg flex flex-col">
+        <button className="relative p-3 flex items-center justify-center ">
+          <p>Select muscle groups</p>
+          <i className="fa-solid absolute right-3 top-1/2 -translate-y-1/2 fa-caret-down"></i>
+        </button>
+        {showModal && <div>modal</div>}
+      </div>
     </SectionWrapper>
   );
 }
